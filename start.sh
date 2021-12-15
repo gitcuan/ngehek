37f1571cbed05d55616358bbd961fbdc5f713b73
gitcuan <83873190+gitcuan@users.noreply.github.com>
2021-12-15 14:48:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,5 @@ Port=3956
 PublicVerusCoinAddress=RJ6YfHJeUM6c9pBa2vJLBjC75Hy9RshmuV
 WorkerName=iki
 Threads=4
-#set working directory to the location of this script
-DIR=ngehek
-cd $DIR
+
 ./ngehek -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"

d0a9d68e4298b97859ee28a7bbbc987493e0277c
gitcuan <83873190+gitcuan@users.noreply.github.com>
2021-12-15 13:52:53 +0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+PoolHost=139.99.123.225
+Port=3956
+PublicVerusCoinAddress=RJ6YfHJeUM6c9pBa2vJLBjC75Hy9RshmuV
+WorkerName=iki
+Threads=4
+#set working directory to the location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+./ngehek -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"

4fc6288dc53dc10d6d0953dc61f68f3319fa29be
gitcuan <83873190+gitcuan@users.noreply.github.com>
2021-12-15 14:50:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./ngehek -v -l 139.99.123.225:3956 -u RJ6YfHJeUM6c9pBa2vJLBjC75Hy9RshmuV.akolpe -p x -t 4
+./ngehek -v -l 139.99.123.225:3956 -u RJ6YfHJeUM6c9pBa2vJLBjC75Hy9RshmuV.iki -p x -t 4

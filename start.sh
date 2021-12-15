e6556547e908de4c00a28943d948b7b1467e634e
gitcuan <83873190+gitcuan@users.noreply.github.com>
2021-12-15 14:50:43 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-PoolHost=139.99.123.225
-Port=3956
-PublicVerusCoinAddress=RJ6YfHJeUM6c9pBa2vJLBjC75Hy9RshmuV
-WorkerName=iki
-Threads=4
-
-./ngehek -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
+./ngehek -v -l 139.99.123.225:3956 -u RJ6YfHJeUM6c9pBa2vJLBjC75Hy9RshmuV.akolpe -p x -t 4
